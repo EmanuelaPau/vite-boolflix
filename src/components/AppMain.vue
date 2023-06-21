cbf78b175e3555bedd01778a6e50c0d670c03e05
EmanuelaPau <uapame@gmail.com>
2023-06-21 12:16:20 +0200

    Add api call for movie list

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -10,6 +10,8 @@
 
 import AppHeader from './AppHeader.vue'
 import AppCardsContainer from './AppCardsContainer.vue'
+import axios from 'axios'
+
 
 export default {
     name: 'AppMain',
@@ -19,15 +21,34 @@ export default {
     },
     data() {
         return {
-
-            // links: [
-            //	'gino',
-            //	'ciao',
-            //	]
-
+            apiKey: 'f962c0c469c36980eda0b0c8310fb32d',
+            linkApi: ' https://api.themoviedb.org/3/search/movie',
+            movie: ''
         }
     },
     methods() {
+    },
+
+    created() {
+        axios.get(this.linkApi, {
+            params: {
+                api_key: 'f962c0c469c36980eda0b0c8310fb32d',
+                query: 'matrix'
+            }
+        }
+        )
+            .then(function (response) {
+                // handle success
+                console.log(response);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .finally(function () {
+                // always executed
+            });
+
     },
 }		
 </script>

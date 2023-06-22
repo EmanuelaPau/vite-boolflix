62851ed78618caca31cbeec0d41245cc58b9cc5b
EmanuelaPau <uapame@gmail.com>
2023-06-22 15:56:02 +0200

    Add links to header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,14 +2,14 @@
     <header class="d-flex justify-content-between align-items-center">
         <!-- header left  -->
         <div class="header-left d-flex justify-content-between align-items-center">
-            <img id="bool-logo" class="me-4" src="../../public/boolflix_logo.png" alt="boolflix logo">
+            <a href="#"><img id="bool-logo" class="me-4" src="../../public/boolflix_logo.png" alt="boolflix logo"></a>
             <ul class="navlist list-unstyled d-flex align-items-center m-0">
-                <li class="m-2">Home</li>
-                <li class="m-2">Serie TV</li>
-                <li class="m-2">Film</li>
-                <li class="m-2">Nuovi e Popolari</li>
-                <li class="m-2">La mia nuova lista</li>
-                <li class="m-2">Sfoglia per lingua</li>
+                <li class="m-2"><a href="">Home</a></li>
+                <li class="m-2"><a href="">Serie TV</a></li>
+                <li class="m-2"><a href="">Film</a></li>
+                <li class="m-2"><a href="">Nuovi e Popolari</a></li>
+                <li class="m-2"><a href="">La mia nuova lista</a></li>
+                <li class="m-2"><a href="">Sfoglia per lingua</a></li>
             </ul>
         </div>
         <!-- header right -->
@@ -23,19 +23,19 @@
                 <input class="my-searchbar me-3" type="text" placeholder="Titoli, persone, generi" aria-label="Search"
                     v-model="searchedText" @keyup.enter="$emit('search', searchedText), searchedText = ''">
                 <a class="text-reset me-3 text-decoration-none " href="#">Bambini</a>
-                <i class="bell fa-regular fa-bell me-3"></i>
-                <img id="profile-pic" class="me-2" src="../../public/profile_pic_1.png" alt="profile pic">
-                <i class="fa-solid fa-sort-down"></i>
+                <a class="text-reset" href=""><i class="bell fa-regular fa-bell me-3"></i></a>
+                <div class="account-menu position-relative">
+                    <img id="profile-pic" class="me-2" src="../../public/profile_pic_1.png" alt="profile pic">
+                    <a class="text-reset" href=""><i class="fa-solid fa-sort-down arrow"></i></a>
+                    <div class="account-accordion-menu">
+
+                    </div>
+
+                </div>
             </div>
         </div>
 
-        <!-- Searchbar -->
-        <!-- <div class="d-flex">
-            <input class="form-control mr-sm-2 w-50" type="text" placeholder="Search" aria-label="Search"
-                v-model="searchedText" @keyup.enter="$emit('search', searchedText), searchedText = ''">
-            <button @click="$emit('search', searchedText), searchedText = ''" class="btn btn-primary"
-                type="button">Ciao</button>
-        </div> -->
+
     </header>
 </template>
 
@@ -71,6 +71,13 @@ img#bool-logo {
 
 ul.navlist {
     height: 100%;
+
+    li {
+        a {
+            text-decoration: none;
+            color: white;
+        }
+    }
 }
 
 button.my-searchbar-button {
@@ -99,5 +106,31 @@ i.bell {
 img#profile-pic {
     height: 30px;
     border-radius: 4px;
+}
+
+.account-menu {
+    cursor: pointer;
+}
+
+.account-menu:hover a i {
+    // color: red;
+    transform-origin: center;
+    transform: rotate(180deg);
+}
+
+div.account-accordion-menu {
+    position: absolute;
+    top: 50px;
+    right: 0;
+    // height: 500px;
+    width: 250px;
+    background-color: aqua;
+
+}
+
+i.arrow {
+    height: 1rem;
+    display: inline-block;
+    transition: .3s all;
 }
 </style> 
